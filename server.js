82a094f081e86b2e4d57a2551723eacd28addf48
= <=>
2021-10-22 15:55:06 -0500

    added CRUD routes for the party table

--- server.js
+++ server.js
@@ -64,17 +64,18 @@ app.get('/api/candidates', (req, res) => {
     });
 });
 
+
 //create a query that gets a candidate based on id for single candidate
 //(the route end point, paramaters, create function)
 app.get('/api/candidate/:id', (req, res) => {
     //set command in a variable for the query
     //use JOIN to add information from another table and where it will be placed with LEFT JOIN
     const sql = `SELECT candidates.*, parties.name 
-                 AS party_name 
-                 FROM candidates 
-                 LEFT JOIN parties 
-                 ON candidates.party_id = parties.id 
-                 WHERE candidates.id = ?`;
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id 
+    WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     //(command,arguments, create function)
@@ -92,26 +93,111 @@ app.get('/api/candidate/:id', (req, res) => {
     });
 });
 
+
+//Update a candidate's party using put() method 
+app.put('/api/candidate/:id', (req, res) => {
+    //this inputCheck forces our user to include a party_id in any PUT request
+    const errors = inputCheck(req.body, 'party_id');
+    if(errors) {
+        res.status(400).json({ error: errors});
+        return;
+    }
+
+    const sql = `UPDATE candidates SET party.id = ?
+                 WHERE id = ? `;
+    const params = [req.body.party_id, req.params.id];
+
+    db.query(sql,params, (err, result) =>{
+        if(err) {
+            res.status(400).json({ error: err.message});
+            //check if a record was found
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        }else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
+
 //Delete a candidate based on id using the delete() method 
 // (endpoint with id, paramaters, create function)
-app.delete('api/candidate/:id', (req,res) =>{
+app.delete('api/candidate/:id', (req, res) => {
     // command created as a variable
     const sql = `DELETE FROM candidates WHERE id = ?`;
     // set parameters as a variable 
     const params = [req.params.id];
     //run the query with the variables 
-    db.query(sql, params, (err,result) => {
+    db.query(sql, params, (err, result) => {
         if (err) {
             //place error message in the json object 
-            res.statusMessage(400).json({ error: res.message});
-        // conditional if user tries to delete a row that isn't there 
-        } else if (!result.affectedRows){
+            res.statusMessage(400).json({ error: res.message });
+            // conditional if user tries to delete a row that isn't there 
+        } else if (!result.affectedRows) {
             res.json({
                 message: 'Candidate not found'
             });
         } else {
-            res.json ({
+            res.json({
                 message: ' deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+//routes for party table 
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+//route for the party id 
+app.get('/api/parties/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ? `;
+    const params = [req.params.id];
+    //using the variables as our parameters 
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+app.delete('/api/parties/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Party not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
                 changes: result.affectedRows,
                 id: req.params.id
             });
@@ -121,19 +207,19 @@ app.delete('api/candidate/:id', (req,res) =>{
 
 //Create a candidate based on id using the post() method
 //(endpoint, parameters using using the req.body for the data using object deconstruction, new function)
-app.post('/api/candidate',({body},res) => {
+app.post('/api/candidate', ({ body }, res) => {
     //used a module called inputCheck to validate user info
-    const errors = inputCheck(body, 'first_name','last_name','industry_connected');
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
     if (errors) {
-        res.status(400).json({error:errors});
+        res.status(400).json({ error: errors });
         return;
     }
     const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) VALUES(?,?,?)`;
     const params = [body.first_name, body.last_name, body.industry_connected];
-    
-    db.query( sql,params,(err,result) =>{
-        if (err){
-            res.status(400).json({error:err.message});
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
             return;
 
         }
